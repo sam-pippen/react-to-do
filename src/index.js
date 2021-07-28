d426fe4a2ebbad351e23079ddf25f3bbc493ee84
sam-pippen <samfpippen@gmail.com>
2021-07-28 08:59:41 -0700

    Update index.js
    updating

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { HashRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom"
 
 //component file
 import TodoContainer from "./functionBased/components/TodoContainer"
@@ -9,9 +9,11 @@ import TodoContainer from "./functionBased/components/TodoContainer"
 import "./functionBased/App.css"
 
 //wrap top-level app in <Router> so that you can navigate to other pages within app
+//basename is the subdirectory name (which is the repository name) i.e. "react-to-do"
+//this was specified in the homepage property in our package.json
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router baseName={process.env.PUBLIC_URL}>
       <TodoContainer />
     </Router>
   </React.StrictMode>,
